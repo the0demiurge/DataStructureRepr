f53ab686cbecf7f31aaf076f887b5f9ebfe8f9b3
Charles Xu <charl3s.xu@gmail.com>
2018-05-23 21:20:03 +0800

    added TreeNode

--- README.md
+++ README.md
@@ -6,8 +6,7 @@ I opened this project to show datastructures by hacking `__repr__` function of P
 
 
 ```python
-from dsr import BinaryTreeNode
-
+print('BinaryTreeNode:')
 a = BinaryTreeNode(100)
 b = BinaryTreeNode(2)
 c = BinaryTreeNode(0, a, b)
@@ -16,15 +15,37 @@ a.right = d
 a.left = d
 
 print(d)
+print('TreeNode:')
+root = TreeNode('tree', [
+    TreeNode('types', [TreeNode(str), TreeNode(int)]),
+    TreeNode('values', [TreeNode(1), TreeNode(3.1415926), TreeNode(True)]),
+    TreeNode('empty'),
+    2.718281828,
+    'Not TreeNode'
+])
+print(root)
 ```
-
+BinaryTreeNode
 ```
-                'a'                
-               /   \               
-          0                   0    
-         / \                 / \   
- 100         2       100         2 
-    \                   \          
-     ...                 ...       
-
+                      ('a')                      
+                     /     \                     
+              (0)                         (0)    
+             /   \                       /   \   
+    (100)         (2)           (100)         (2)
+   /     \                     /     \           
+...       ...               ...       ...        
+```
+TreeNode
+```
+('tree')
+ ├── ('types')
+ │    ├── (<class 'str'>)
+ │    └── (<class 'int'>)
+ ├── ('values')
+ │    ├── (1)
+ │    ├── (3.1415926)
+ │    └── (True)
+ ├── ('empty')
+ ├── 2.718281828
+ └── 'Not TreeNode'
 ```
--- dsr.py
+++ dsr.py
@@ -42,3 +42,53 @@ class BinaryTreeNode(object):
             zip(repr_left, repr_right)))
 
         return '\n'.join(result)
+
+
+class TreeNode(object):
+    __slots__ = ("value", "children")
+
+    def __init__(self, value=None, children=None):
+        self.value = value
+        self.children = list() if children is None else children
+
+    @recursive_repr(fillvalue='...')
+    def __repr__(self):
+        root = '(' + repr(self.value) + ')'
+        if not self.children:
+            return root
+        result = [root]
+        for child in self.children[:-1]:
+            result.append(self.__build_child_repr(child, end=False))
+        result.append(self.__build_child_repr(self.children[-1], end=True))
+        return '\n'.join(result)
+
+    @staticmethod
+    def __build_child_repr(child, end=False):
+        child_repr = repr(child).split('\n')
+        fork, space = (' └── ', '     ') if end else (' ├── ', ' │   ')
+        return '\n'.join([fork + child_repr[0]] + list(map(lambda x: space + x, child_repr[1:])))
+
+
+def main():
+    print('BinaryTreeNode:')
+    a = BinaryTreeNode(100)
+    b = BinaryTreeNode(2)
+    c = BinaryTreeNode(0, a, b)
+    d = BinaryTreeNode('a', c, c)
+    a.right = d
+    a.left = d
+
+    print(d)
+    print('TreeNode:')
+    root = TreeNode('tree', [
+        TreeNode('types', [TreeNode(str), TreeNode(int)]),
+        TreeNode('values', [TreeNode(1), TreeNode(3.1415926), TreeNode(True)]),
+        TreeNode('empty'),
+        2.718281828,
+        'Not TreeNode'
+    ])
+    print(root)
+
+
+if __name__ == '__main__':
+    main()
