d26e72cc0b425606977ecd1c5c480a5f20a66a87
Charles <charl3s.xu@gmail.com>
2020-08-03 01:08:42 +0800

    .

--- dsr/trees.py
+++ dsr/trees.py
@@ -27,8 +27,8 @@ class BinaryTreeNode(object):
             (repr_left, repr_right, repr_self))
 
         diff_height = height_left - height_right
-        left_bar = ' ' if self.left is None else '🡗'
-        right_bar = ' ' if self.right is None else '🡖'
+        left_bar = ' ' if self.left is None else '↙'
+        right_bar = ' ' if self.right is None else '↘'
 
         if diff_height > 0:
             repr_right.extend([' ' * len_right] * diff_height)
